0fbe2e6363c0594eae0bc1117f8c282c244bfd6c
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-12 22:53:40 +0600

    Triangle

---
+++ Main.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("white")
+turtle.Screen().setup(500,500)
+pencil = turtle.Turtle()
+for i in range(3):
+    pencil.forward(100)
+    pencil.left(120)
+
+
+
+turtle.done()
