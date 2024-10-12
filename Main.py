baf8a6a249d89b3246cab1dca09b7eacd58793f7
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-12 23:03:45 +0600

    Devil

--- Main.py
+++ Main.py
@@ -5,7 +5,14 @@ pencil = turtle.Turtle()
 for i in range(3):
     pencil.forward(100)
     pencil.left(120)
-
+pencil.penup()
+pencil.left(90)
+pencil.forward(50)
+pencil.pendown()
+pencil.right(90)
+for i in range(3):
+    pencil.forward(100)
+    pencil.right(120)
 
 
 turtle.done()
