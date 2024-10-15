c33d65f716078b3595c617a5fb555a586949beb7
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-15 11:21:35 +0600

    Devil

--- Main.py
+++ Main.py
@@ -1,17 +1,17 @@
 import turtle
 turtle.Screen().bgcolor("white")
-turtle.Screen().setup(500,500)
+turtle.Screen().setup(1000,600)
 pencil = turtle.Turtle()
 for i in range(3):
-    pencil.forward(100)
+    pencil.forward(300)
     pencil.left(120)
 pencil.penup()
 pencil.left(90)
-pencil.forward(50)
+pencil.forward(150)
 pencil.pendown()
 pencil.right(90)
 for i in range(3):
-    pencil.forward(100)
+    pencil.forward(300)
     pencil.right(120)
 
 
